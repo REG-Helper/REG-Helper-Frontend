ace9606e7e11a66819b824ff3c06f644c6d6be2f
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-10-15 03:11:30 +0700

    feat: change redirect url

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -3,12 +3,14 @@ import Link from 'next/link';
 
 export function Footer() {
   return (
-    <footer className="bg-gray-300 p-4 text-center py-16">
+    <footer className="bg-gray-300 p-4 py-16 text-center">
       <h6 className="text-4xl font-medium">REG Helper</h6>
-      <div className="flex items-center justify-center mt-4">
+      <div className="mt-4 flex items-center justify-center">
         <p className="mr-2 text-lg text-gray-600">Source code available on</p>
         <Link
-          href="https://github.com/example"
+          href="https://github.com/orgs/REG-Helper/repositories"
+          target="_blank"
+          rel="noopener noreferrer"
           className="text-4xl text-blue-500"
         >
           <Icon icon="mdi:github" />
