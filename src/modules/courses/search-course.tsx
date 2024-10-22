8f44bba2048f2198f8acf8bb3343cf6a1734cfcf
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-10-23 02:38:01 +0700

    feat: add icon in search course component

--- src/modules/courses/search-course.tsx
+++ src/modules/courses/search-course.tsx
@@ -1,23 +1,47 @@
 'use client';
 
 import { Input } from '@/shared/components/ui/input';
-import { type ChangeEvent } from 'react';
+import { useCallback, type ChangeEvent } from 'react';
 import { useCourseStore } from './_store';
+import { Button } from '@/shared/components/ui/button';
+import { Icon } from '@iconify/react';
 
 export function SearchCourse() {
   const { setSearch } = useCourseStore((state) => state.actions);
+  const searchTerm = useCourseStore((state) => state.search);
 
-  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
-    setSearch(e.target.value);
-  };
+  const handleChange = useCallback(
+    (e: ChangeEvent<HTMLInputElement>) => {
+      setSearch(e.target.value);
+    },
+    [setSearch],
+  );
+
+  const clearSearch = useCallback(() => {
+    setSearch('');
+  }, [setSearch]);
 
   return (
-    <div>
+    <div className="relative">
       <Input
+        value={searchTerm}
         onChange={handleChange}
         placeholder="ค้นหาด้วยรหัสวิชา / ชื่อวิชา"
         className="py-5 text-base font-medium"
       />
+      <Button
+        variant="ghost"
+        size="icon"
+        className="absolute right-2 top-1/2 -translate-y-1/2 transform rounded-full text-gray-400"
+        onClick={searchTerm ? clearSearch : undefined}
+      >
+        <Icon
+          icon={
+            searchTerm ? 'material-symbols:close' : 'material-symbols:search'
+          }
+          className="text-2xl"
+        />
+      </Button>
     </div>
   );
 }
