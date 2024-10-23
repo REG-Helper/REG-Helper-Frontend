e7232a40271402013e3072c5cca6f2efbd7db86b
thanpisitsu <Thanpisitsuriyaroj@gmail.com>
2024-10-23 23:27:01 +0700

    clean code

--- src/app/(dashboard)/courses/[slug]/page.tsx
+++ src/app/(dashboard)/courses/[slug]/page.tsx
@@ -4,12 +4,21 @@ import { CourseDetail } from '@/modules/courses/course-detail';
 import { buttonVariants } from '@/shared/components/ui/button';
 import { paths } from '@/shared/routes';
 import Link from 'next/link';
+import { useEffect, useState } from 'react';
 
 export default function CoursePage({ params }: { params: { slug: string } }) {
-  const url = new URL(window.location.href);
-  const param = new URLSearchParams(url.search);
-  const year = param.get('year') ? (param.get('year') as string) : '';
-  const semester = param.get('semester') ? (param.get('semester') as string) : '';
+  const [year, setYear] = useState('');
+  const [semester, setSemester] = useState('');
+
+  useEffect(() => {
+    const url = new URL(window.location.href);
+    const param = new URLSearchParams(url.search);
+    const yearParam = param.get('year');
+    const semesterParam = param.get('semester');
+
+    setYear(yearParam ? yearParam : '');
+    setSemester(semesterParam ? semesterParam : '');
+  }, []);
 
   return (
     <div className="mx-auto space-y-8 px-4 pb-4 md:px-8 xl:max-w-screen-xl">
