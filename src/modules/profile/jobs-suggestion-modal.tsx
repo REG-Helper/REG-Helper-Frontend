872ab038d743d82f46fcbc0ebb0320aebd556351
Kittipod Lambangchang <tonkittipod@gmail.com>
2024-10-29 08:10:38 +0700

    feat: remove unused code

--- src/modules/profile/jobs-suggestion-modal.tsx
+++ src/modules/profile/jobs-suggestion-modal.tsx
@@ -11,64 +11,7 @@ import {
 } from '@/shared/components/ui/dialog';
 import { useGetTopJobs } from './_hooks';
 import { Button } from '@/shared/components/ui/button';
-import type { TopJob } from './_types';
-import { cn } from '@/shared/utils';
-
-// ======================================================================================================
-
-const getMedalIcon = (rank: number) => {
-  switch (rank) {
-    case 1:
-      return '🥇';
-    case 2:
-      return '🥈';
-    case 3:
-      return '🥉';
-    default:
-      return '';
-  }
-};
-
-type RankCardProps = {
-  job: TopJob & {
-    rank: number;
-  };
-};
-
-export function RankCard({ job }: RankCardProps) {
-  return (
-    <div
-      className={cn(
-        'flex w-full max-w-[200px] flex-col items-center justify-center rounded-lg border border-gray-300 p-4',
-        { '-translate-y-8 transform shadow-lg': job.rank === 1 },
-      )}
-    >
-      <div className="mb-2 flex h-16 w-16 aspect-square items-center justify-center rounded-full bg-gray-200">
-        <span className="text-2xl font-bold">{getMedalIcon(job.rank)}</span>
-      </div>
-      <div className="mt-4">
-        <h3 className="text-center text-lg font-semibold">{job.nameEn}</h3>
-        <p className="text-center text-gray-500">{job.nameTh}</p>
-      </div>
-    </div>
-  );
-}
-
-export function RankCardMobile({ job }: RankCardProps) {
-  return (
-    <div className="flex items-center border-b p-4 last:border-b-0">
-      <div className="flex h-8 w-8 aspect-square items-center justify-center rounded-full bg-gray-200">
-        <span className="font-bold">{getMedalIcon(job.rank)}</span>
-      </div>
-      <div className="ml-4">
-        <h3 className="text-lg font-semibold">{job.nameEn}</h3>
-        <p className="text-gray-500">{job.nameTh}</p>
-      </div>
-    </div>
-  );
-}
-
-// ======================================================================================================
+import { RankCard, RankCardMobile } from './rank-card';
 
 export function JobsSuggestionModal() {
   const { data: topJobs } = useGetTopJobs();
