b0809ad2a67f7d786d992e14f115362e0cba67e9
Naphat Phatinawin <mick.naphatt@gmail.com>
2024-10-30 18:55:21 +0700

    feat: footer color

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -3,15 +3,15 @@ import Link from 'next/link';
 
 export function Footer() {
   return (
-    <footer className="bg-gray-300 p-4 py-16 text-center">
-      <h6 className="text-4xl font-medium">REG Helper</h6>
+    <footer className="bg-deep-blue-light p-4 py-16 text-center">
+      <h6 className="text-4xl font-medium text-white">REG Helper</h6>
       <div className="mt-4 flex items-center justify-center">
-        <p className="mr-2 text-lg text-gray-600">Source code available on</p>
+        <p className="mr-2 text-lg text-light-gray">Source code available on</p>
         <Link
           href="https://github.com/orgs/REG-Helper/repositories"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-4xl text-blue-500"
+          className="text-4xl text-light-gray"
         >
           <Icon icon="mdi:github" />
         </Link>
