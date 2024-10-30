213cab43a21af12cbc1db047cd7157d8944c1089
Naphat Phatinawin <mick.naphatt@gmail.com>
2024-10-30 19:12:31 +0700

    feat: footer color

--- src/shared/components/footer.tsx
+++ src/shared/components/footer.tsx
@@ -11,7 +11,7 @@ export function Footer() {
           href="https://github.com/orgs/REG-Helper/repositories"
           target="_blank"
           rel="noopener noreferrer"
-          className="text-4xl text-white"
+          className="text-4xl text-light-gray"
         >
           <Icon icon="mdi:github" />
         </Link>
